a27b009cc6d95e64b6e7faf044636a30ed8ab5a3
duchuykg <duchuyhskg@gmail.com>
2023-09-13 11:09:14 +0700

    init

--- main.py
+++ main.py
@@ -1,17 +1,328 @@
-import pygame, sys
-from pygame import *
+# import pygame, sys
+# from pygame import *
 
+# pygame.init()
+
+# pygame.display.set_caption("Zombies Smash")
+# logo = pygame.image.load(r'img\logo.webp')
+# bg = pygame.image.load(r'img\bg.jpg')
+# pygame.display.set_icon(logo)
+# screen = pygame.display.set_mode((1200, 673))
+# while True:
+#     for event in pygame.event.get():
+#         if event.type == QUIT:
+#             pygame.quit()
+#             sys.exit()
+#         screen.blit(bg,(0,0))
+#         pygame.display.update()
+
+import pygame
+import random
+
+from pygame import *
+import math as mp
+import asyncio
+
+
+class GameManager:
+    def __init__(self):
+        # Define constants
+        # self.SCREEN_WIDTH = 800
+        # self.SCREEN_HEIGHT = 600
+        self.SCREEN_WIDTH = 1200
+        self.SCREEN_HEIGHT = 670
+        self.FPS = 60
+        self.MOLE_WIDTH = 90
+        self.MOLE_HEIGHT = 81
+        self.FONT_SIZE = 31
+        self.FONT_TOP_MARGIN = 26
+        self.LEVEL_SCORE_GAP = 4
+        self.LEFT_MOUSE_BUTTON = 1
+        self.GAME_TITLE = "Headshot zombies - Assignment 1"
+        
+        # Initialize player's score, number of missed hits and level
+        self.score = 0
+        self.misses = 0
+        self.level = 1
+        # Initialize screen
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT))
+        pygame.display.set_caption(self.GAME_TITLE)
+
+        # self.background = pygame.image.load("images/4x/bg.png")
+        self.background = pygame.image.load("img/bg3.png")
+        # self.bus = pygame.image.load("images/bus.png").convert_alpha()
+        self.pos_x = self.SCREEN_WIDTH -self.background.get_width()
+        self.pos_y = 0
+        self.speed_background = 0
+
+        # Font object for displaying text
+        self.font_obj = pygame.font.Font('./fonts/Purple Smile.ttf', self.FONT_SIZE)
+        # Initialize the mole's sprite sheet
+        # 6 different states
+        sprite_sheet = pygame.image.load("img/mole-01.png")
+        self.mole = []
+        self.mole.append(sprite_sheet.subsurface(270, 0, 120, 100))
+        self.mole.append(sprite_sheet.subsurface(310, 0, 120, 100))
+        self.mole.append(sprite_sheet.subsurface(455, 0, 120, 100))
+        self.mole.append(sprite_sheet.subsurface(580, 0, 120, 100))
+        self.mole.append(sprite_sheet.subsurface(722, 0, 120, 100))
+        self.mole.append(sprite_sheet.subsurface(853, 0, 120, 100))
+        self.mole.append(sprite_sheet.subsurface(853, 0, 120, 100))
+        # self.mole.append(sprite_sheet.subsurface(169, 0, 90, 81))
+        # self.mole.append(sprite_sheet.subsurface(309, 0, 90, 81))
+        # self.mole.append(sprite_sheet.subsurface(449, 0, 90, 81))
+        # self.mole.append(sprite_sheet.subsurface(575, 0, 116, 81))
+        # self.mole.append(sprite_sheet.subsurface(717, 0, 116, 81))
+        # self.mole.append(sprite_sheet.subsurface(853, 0, 116, 81))
+        # self.mole.append(sprite_sheet.subsurface(853, 0, 116, 81))
+        # Positions of the holes in background
+        self.hole_positions = []
+        # self.hole_positions.append((100, 227))
+        # self.hole_positions.append((200,227))
+        # self.hole_positions.append((285, 227))
+        # self.hole_positions.append((443, 227))
+        # self.hole_positions.append((534, 227))
+        # self.hole_positions.append((622, 227))
+        # self.hole_positions.append((104,365))
+        # self.hole_positions.append((205,365))
+        # self.hole_positions.append((293, 365))
+        # self.hole_positions.append((448, 365))
+        # self.hole_positions.append((539, 365))
+
+        self.hole_positions.append((383, 481))
+        self.hole_positions.append((475, 525))
+        self.hole_positions.append((575, 525))
+        self.hole_positions.append((720, 525))
+        self.hole_positions.append((820, 525))
+        self.hole_positions.append((900, 525))
+        self.hole_positions.append((380, 660))
+        self.hole_positions.append((475, 660))
+        self.hole_positions.append((575, 660))
+        self.hole_positions.append((720, 660))
+        self.hole_positions.append((820, 663))
+
+        # for i in range(11):
+        #     self.hole_positions.append((820, 663))
+        # Init debugger
+        self.debugger = Debugger("debug")
+        # Sound effects
+        self.soundEffect = SoundEffect()
+
+    # Calculate the player level according to his current score & the LEVEL_SCORE_GAP constant
+    def get_player_level(self):
+        newLevel = 1 + int(self.score / self.LEVEL_SCORE_GAP)
+        if newLevel != self.level:
+            # if player get a new level play this sound
+            self.soundEffect.playLevelUp()
+        return 1 + int(self.score / self.LEVEL_SCORE_GAP)
+
+    # Get the new duration between the time the mole pop up and down the holes
+    # It's in inverse ratio to the player's current level
+    def get_interval_by_level(self, initial_interval):
+        new_interval = initial_interval - self.level * 0.15
+        if new_interval > 0:
+            return new_interval
+        else:
+            return 0.05
+
+    # Check whether the mouse click hit the mole or not
+    def is_mole_hit(self, mouse_position, current_hole_position):
+        mouse_x = mouse_position[0]
+        mouse_y = mouse_position[1]
+        current_hole_x = current_hole_position[0]
+        current_hole_y = current_hole_position[1]
+        if (mouse_x > current_hole_x) and (mouse_x < current_hole_x + self.MOLE_WIDTH) and (mouse_y > current_hole_y) and (mouse_y < current_hole_y + self.MOLE_HEIGHT):
+            return True
+        else:
+            return False
+
+    def draw_background(self):
+        self.screen.blit(self.background, (int(self.pos_x), int(self.pos_y)))
+        self.screen.blit(self.background, (int(self.pos_x - self.background.get_width()), int(self.pos_y)))
+    def update_background(self):      
+        self.draw_background()  
+        self.pos_x += self.speed_background
+        if self.pos_x > self.SCREEN_WIDTH:
+            self.pos_x = self.SCREEN_WIDTH - self.background.get_width()
+
+
+    # Update the game states, re-calculate the player's score, misses, level
+    def update(self):
+        # Update the player's score
+        current_score_string = "SCORE: " + str(self.score)
+        score_text = self.font_obj.render(current_score_string, True, (255, 255, 255))
+        score_text_pos = score_text.get_rect()
+        score_text_pos.centerx = self.SCREEN_WIDTH / 5 * 4
+        score_text_pos.centery = self.FONT_TOP_MARGIN
+        self.screen.blit(score_text, score_text_pos)
+        # Update the player's misses
+        current_misses_string = "MISSES: " + str(self.misses)
+        misses_text = self.font_obj.render(current_misses_string, True, (255, 255, 255))
+        misses_text_pos = misses_text.get_rect()
+        misses_text_pos.centerx = self.SCREEN_WIDTH / 5 * 3
+        misses_text_pos.centery = self.FONT_TOP_MARGIN
+        self.screen.blit(misses_text, misses_text_pos)
+        # Update the player's level
+        current_level_string = "LEVEL: " + str(self.level)
+        level_text = self.font_obj.render(current_level_string, True, (255, 255, 255))
+        level_text_pos = level_text.get_rect()
+        level_text_pos.centerx = self.SCREEN_WIDTH / 5 * 1
+        level_text_pos.centery = self.FONT_TOP_MARGIN
+        self.screen.blit(level_text, level_text_pos)
+
+    # Start the game's main loop
+    # Contains some logic for handling animations, mole hit events, etc..
+    async def start(self):
+        cycle_time = 0
+        num = -1
+        loop = True
+        is_down = False
+        interval = 0.1
+        initial_interval = 1
+        frame_num = 0
+        left = 0
+        # Time control variables
+        clock = pygame.time.Clock()
+        pic = None
+        for i in range(len(self.mole)):
+            self.mole[i].set_colorkey((0, 0, 0))
+            self.mole[i] = self.mole[i].convert_alpha()
+
+        while loop:
+            # print(self.TILES)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    loop = False
+                if event.type == MOUSEBUTTONDOWN and event.button == self.LEFT_MOUSE_BUTTON:
+                    self.soundEffect.playFire()
+                    if self.is_mole_hit(mouse.get_pos(), self.hole_positions[frame_num]) and num > 0 and left == 0:
+                        num = 3
+                        left = 14
+                        is_down = False
+                        interval = 0
+                        self.score += 1  # Increase player's score
+                        self.level = self.get_player_level()  # Calculate player's level
+                        # Stop popping sound effect
+                        self.soundEffect.stopPop()
+                        # Play hurt sound
+                        self.soundEffect.playHurt()
+                        self.update()
+                    else:
+                        self.misses += 1
+                        self.update()
+
+            if (pygame.mixer.music.get_pos() / 1000 > 39):
+                self.speed_background = 10
+            mil = clock.tick(self.FPS)
+            self.screen.fill((0, 0, 0))
+            self.update_background()
+            # self.screen.blit(self.bus, (random.randrange(299, 302, 3), 300))
+            if pic is not None:
+                self.screen.blit(pic, (self.hole_positions[frame_num][0] - left, self.hole_positions[frame_num][1]))
+            self.update()
+            # bg_rect.x = i * bg_width + scroll
+            # pygame.draw.rect(screen, (255, 0, 0), bg_rect, 1)
+            if num > 5:
+                # self.update_background()
+                # self.screen.blit(self.bus, (random.randrange(299, 302, 3), 300))
+                # self.update()
+                num = -1
+                pic = None
+                left = 0
+
+            if num == -1:
+                # self.update_background()
+                # self.screen.blit(self.bus, (random.randrange(299, 302, 3), 300))
+                # self.update()
+                num = 0
+                is_down = False
+                interval = 0.5
+                frame_num = random.randint(0, 10)
+
+                
+            sec = mil / 1000.0
+            cycle_time += sec
+            if cycle_time > interval:
+                # self.update_background()
+                pic = self.mole[num]
+                self.update_background()
+                # self.screen.blit(self.bus, (random.randrange(299, 302, 3), 300))
+                self.screen.blit(pic, (self.hole_positions[frame_num][0] - left, self.hole_positions[frame_num][1]))
+                self.update()
+                if is_down is False:
+                    num += 1
+                else:
+                    num -= 1
+                if num == 4:
+                    interval = 0.3
+                elif num == 3:
+                    num -= 1
+                    is_down = True
+                    self.soundEffect.playPop()
+                    interval = self.get_interval_by_level(initial_interval)  # get the newly decreased interval value
+                else:
+                    interval = 0.1
+                cycle_time = 0
+            # Update the display
+            pygame.display.flip()
+            await asyncio.sleep(0)
+
+
+
+
+
+# The Debugger class - use this class for printing out debugging information
+class Debugger:
+    def __init__(self, mode):
+        self.mode = mode
+
+    def log(self, message):
+        if self.mode == "debug":
+            print("> DEBUG: " + str(message))
+
+
+class SoundEffect:
+    def __init__(self):
+        self.mainTrack = pygame.mixer.music.load("sounds/themesong.wav")
+        self.fireSound = pygame.mixer.Sound("sounds/fire.wav")
+        self.fireSound.set_volume(1.0)
+        self.popSound = pygame.mixer.Sound("sounds/pop.wav")
+        self.hurtSound = pygame.mixer.Sound("sounds/hurt.wav")
+        self.levelSound = pygame.mixer.Sound("sounds/point.wav")
+        pygame.mixer.music.play(-1)
+
+    def playFire(self):
+        self.fireSound.play()
+
+    def stopFire(self):
+        self.fireSound.sop()
+
+    def playPop(self):
+        self.popSound.play()
+
+    def stopPop(self):
+        self.popSound.stop()
+
+    def playHurt(self):
+        self.hurtSound.play()
+
+    def stopHurt(self):
+        self.hurtSound.stop()
+
+    def playLevelUp(self):
+        self.levelSound.play()
+
+    def stopLevelUp(self):
+        self.levelSound.stop()
+
+###############################################################
+# Initialize the game
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=512)
 pygame.init()
 
-pygame.display.set_caption("Zombies Smash")
-logo = pygame.image.load(r'img\logo.webp')
-bg = pygame.image.load(r'img\bg.jpg')
-pygame.display.set_icon(logo)
-screen = pygame.display.set_mode((1200, 673))
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-        screen.blit(bg,(0,0))
-        pygame.display.update()
+# Run the main loop
+my_game = GameManager()
+asyncio.run(my_game.start())
+# Exit the game if the main loop ends
+pygame.quit()
